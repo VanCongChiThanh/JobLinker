2ae733759a440e672b1c84a474e01e7a78a93ee7
Văn Công Chí Thanh <vancongchithanh2004@gmail.com>
2024-11-13 23:21:14 +0700

    update Spring Security config to allow GET requests without authentication

--- JobLinker/src/main/java/com/joblinker/config/SecurityConfiguration.java
+++ JobLinker/src/main/java/com/joblinker/config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import com.nimbusds.jose.util.Base64;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -44,6 +45,9 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
                                 .requestMatchers("/", "/api/v1/auth/login","/api/v1/auth/refresh","/api/v1/auth/logout","/storage/**").permitAll() // Cho phép truy cập vào tất cả các endpoint liên quan đến users
+                                .requestMatchers(HttpMethod.GET, "/api/v1/companies/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/v1/jobs/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/v1/skills/**").permitAll()
                                 .anyRequest().authenticated() // Các yêu cầu khác sẽ yêu cầu xác thực
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())
